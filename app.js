449fe7521c7bcdcb678eb92efd00850dd4afedf8
Emily Leng <ice_el@sbcglobal.net>
2016-02-18 23:14:21 +0000

    more db stuff

--- app.js
+++ app.js
@@ -51,22 +51,35 @@ app.post("/createGroup", function(req, res) {
 	db("organizers").push(req.body);
 	res.cookie("JoinCode", id);*/
 	var joincode = token(10);
-	
-	db.collection("groups").insert({"gid": joincode, "name": req.body["Name"]}, function(err, doc){
+	db.collection("groups").find({gid: joincode}, function(err, doc){
+		if (doc[0] == "undefined"){
+			db.collection("groups").insert({"gid": joincode, "name": req.body["Name"]}, function(err, doc){
+				console.log(doc[0]);
+				if(err){
+					console.log(err);
+				}
+				var salt = bcrypt.genSaltSync(10);
+				var phash = bcrypt.hashSync(req.body["Password"], salt);
+				
+				var user = {"uid": token(), "gid": joincode, "name": req.body["Name"], "password": phash, "email": req.body["Email"], "wishlist": [req.body["Gift1"], req.body["Gift2"], req.body["Gift3"]], "organizer": true};
+				db.collection("users").insert(user, function(err, doc){
+					console.log(doc[0]);
+					if(err){
+						console.log(err);
+					}
+				});
+			});
+			res.cookie("JoinCode", joincode);
+			res.send({ success: 1 });
+		}
+		else{
+			res.send({ success: 0});
+		}
 		if(err){
 			console.log(err);
 		}
-		var salt = bcrypt.genSaltSync(10);
-		var phash = bcrypt.hashSync(req.body["Password"], salt);
 		
-		var user = {"uid": token(), "gid": joincode, "name": req.body["Name"], "password": phash, "email": req.body["Email"], "wishlist": [req.body["Gift1"], req.body["Gift2"], req.body["Gift3"]], "organizer": true};
-		db.collection("users").insert(user, function(err, doc){
-			if(err){
-				console.log(err);
-			}
-		});
 	});
-	res.send({ success: 1 });
 });
 
 app.post("/joinGroup", function(req, res) {
@@ -74,20 +87,24 @@ app.post("/joinGroup", function(req, res) {
 	var code = req.body["JoinCode"];
 	/*if(db("organizers").find({"JoinCode": code}) != undefined && db("users").find({"JoinCode": code, "Email": email}) == undefined){
 		db("users").push(req.body);*/
-	if(db.collection("groups").find({"gid": code}) != -1){
-		var user = {"uid": token(), "gid": code, "name": req.body["Name"], "email": req.body["Email"], "wishlist": [req.body["Gift1"], req.body["Gift2"], req.body["Gift3"]], "organizer": false};
-		db.collection("users").insert(user, function(err, doc){
-			if(err){
-				console.log(err);
-			}
-		})	
+	db.collection("groups").find({"gid": code}, function(err, doc){
+		if(doc[0] != "undefined"){
+			var user = {"uid": token(), "gid": code, "name": req.body["Name"], "email": req.body["Email"], "wishlist": [req.body["Gift1"], req.body["Gift2"], req.body["Gift3"]], "organizer": false};
+			db.collection("users").insert(user, function(err, doc){
+				if(err){
+					console.log(err);
+				}
+			});	
 		res.cookie("JoinCode", code);
 		res.send({ success: 1 });
-	}
-	else{
-		res.send({ success: 0 });
-	}
-	
+		}
+		else{
+			res.send({ success: 0 });
+		}
+		if(err){
+			console.log(err);
+		}
+	});
 });
 
 app.post("/viewGroup", function(req, res) {
@@ -108,12 +125,22 @@ app.post("/viewGroup", function(req, res) {
 			}
 		}
 		console.log(members);*/
-	var org = db.collection("users").find({});
-		if(org != null){
-		res.send({ success: 1, members: members });
-	}
-	else{
-		res.send({ success: 0});
-	}
+	db.collection("users").find({"joincode": code}).toArray(function(err, doc){
+		if(doc[0] != "undefined"){
+			for (var i = 0; i < doc.length; i++){
+				members.push({
+					"Name": doc[i]["name"],
+					"Email": doc[i]["email"]
+				});
+			}
+			res.send({ success: 1, members: members });
+		}
+		else{
+			res.send({ success: 0});
+		}
+		if(err){
+			console.log(err);
+		}
+	});
 	
 });
